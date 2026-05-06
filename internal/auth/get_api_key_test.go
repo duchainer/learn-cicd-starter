d9dfc1f0c4907abfa30211a2eb0016da4332c9a8
raph <duchainer@protonmail.com>
2026-05-06 00:49:28 -0400

    Ch2-L1 Add an auth test

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+    //"reflect"
+    "testing"
+    "net/http"
+)
+
+func TestEmptyHeader(t *testing.T) {
+    _, error := GetAPIKey(http.Header{})
+    if error == nil{
+        t.Fatalf("Expected to get an error, not %v", error)
+    }
+}
